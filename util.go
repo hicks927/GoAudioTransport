8161f7ec1d55593eba5b3ad0672bc98875716083
hicks927 <isaaclowe1999@gmail.com>
2022-01-28 15:12:12 +0000

    Implement Audio Ring Buffer

--- util.go
+++ util.go
@@ -34,6 +34,7 @@ func bufferToFloat32Arr(buf *bytes.Buffer, sliceLength int) []float32 {
 	return out
 }
 
+//noinspection GoUnusedFunction
 func interleaveFloat32(f [][]float32) []float32 {
 	out := make([]float32, len(f[0])+len(f[1]))
 	outIndex := 0
@@ -44,4 +45,74 @@ func interleaveFloat32(f [][]float32) []float32 {
 		outIndex++
 	}
 	return out
+}
+
+type RingBufferF struct {
+	buffer    [][]float32
+	readIdx   int
+	writeIdx  int
+	size      int
+	readReset bool
+}
+
+func newRingBuffer(size int, channels int) RingBufferF {
+	b := make([][]float32, channels)
+	for i := 0; i < channels; i++ {
+		b[i] = make([]float32, size)
+	}
+
+	readIdx := 0
+	writeIdx := 0
+	return RingBufferF{
+		buffer:    b,
+		readIdx:   readIdx,
+		writeIdx:  writeIdx,
+		size:      size,
+		readReset: true,
+	}
+}
+
+func (r *RingBufferF) writeAudio(samples [][]float32) {
+	arrRemainder := r.size - r.writeIdx
+	if arrRemainder < len(samples[0]) {
+		for i := 0; i < len(samples); i++ {
+			copy(r.buffer[i][:r.writeIdx], samples[i][:arrRemainder])
+			copy(r.buffer[i], samples[i][arrRemainder:])
+		}
+		r.writeIdx = (len(samples[0]) + r.writeIdx) % r.size
+
+	} else {
+		for i := 0; i < len(samples); i++ {
+			copy(r.buffer[i][r.writeIdx:(r.writeIdx+len(samples[0]))], samples[i])
+		}
+		r.writeIdx = r.writeIdx + len(samples[0])
+	}
+}
+
+func (r *RingBufferF) readAudio(samples [][]float32) {
+	arrRemainder := r.size - r.readIdx
+	if arrRemainder < len(samples[0]) {
+		for i := 0; i < len(samples); i++ {
+			copy(samples[i][:arrRemainder], r.buffer[i][r.readIdx:])
+			copy(samples[i][arrRemainder:], r.buffer[i])
+		}
+		r.readIdx = (len(samples[0]) + r.readIdx) % r.size
+	} else {
+		for i := 0; i < len(samples); i++ {
+			copy(samples[i], r.buffer[i][r.readIdx:(r.readIdx+len(samples[0]))])
+		}
+		r.readIdx = r.readIdx + len(samples[0])
+	}
+}
+
+func (r *RingBufferF) setLatency(bufferSamples int) {
+	if r.writeIdx-bufferSamples < 0 {
+		r.readIdx = (r.writeIdx - bufferSamples) + r.size
+	} else {
+		r.readIdx -= bufferSamples
+	}
+}
+
+func (r *RingBufferF) resetReader() {
+	r.readIdx = r.writeIdx
 }
